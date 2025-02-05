c966674bcd3a605b594ff31a3fc480ceed5f8248
Serhii Kamniev <sergey.kamnev@gmail.com>
2025-02-05 16:06:36 +0200

    Compiling variables.ts to see output

---
+++ varibles.js
@@ -0,0 +1,9 @@
+var movieTitle = "Amadeus";
+movieTitle = "Arrival";
+movieTitle = 100;
+movieTitle.toUpperCase();
+var numCatLives = 9;
+numCatLives += 1;
+var gameOver = false;
+gameOver = true;
+gameOver = "true";
