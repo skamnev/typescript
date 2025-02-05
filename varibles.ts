cd47a41ce18d8d92d116be364d9366579cdfac3f
Serhii Kamniev <sergey.kamnev@gmail.com>
2025-02-05 16:04:19 +0200

    Variables to try

---
+++ varibles.ts
@@ -0,0 +1,11 @@
+let movieTitle: string = "Amadeus";
+movieTitle = "Arrival";
+movieTitle = 100;
+movieTitle.toUpperCase();
+
+let numCatLives: number = 9;
+numCatLives +=1;
+
+let gameOver: boolean = false;
+gameOver = true;
+gameOver = "true";
