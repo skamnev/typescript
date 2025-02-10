6d7c6b163b4aedc3ca711362f3808c68685cbefa
Serhii Kamniev <sergey.kamnev@gmail.com>
2025-02-10 10:56:06 +0200

    Working with enums in TS

--- tuplesEnums.ts
+++ tuplesEnums.ts
@@ -3,3 +3,27 @@ const color: [number, number, number] = [255, 0, 255];
 type HTTPResponse = [number, string];
 
 const goodRes: HTTPResponse = [200, "OK"];
+
+enum OrderStatus {
+    PENDING,
+    SHIPPED,
+    DELIVERED,
+    RETURNED
+}
+
+const myStatus = OrderStatus.DELIVERED;
+
+function isDelivered(status: OrderStatus) {
+    return status === OrderStatus.DELIVERED;
+}
+
+isDelivered(3);
+
+enum ArrowKeys {
+    UP = "up",
+    DOWN = "down",
+    LEFT = "left",
+    RIGHT = "right",
+}
+
+ArrowKeys.LEFT;
