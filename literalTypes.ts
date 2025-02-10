c9a415a04ee8422c40ef03d05a3e8864a5c77768
Serhii Kamniev <sergey.kamnev@gmail.com>
2025-02-10 10:26:43 +0200

    Working with literal types in TS

---
+++ literalTypes.ts
@@ -0,0 +1,8 @@
+let zero: 0 = 0;
+
+
+let mood: "Happy" | "Sad" = "Happy";
+mood = "Sad";
+
+type DayOfWeek = "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday" | "Sunday";
+let today: DayOfWeek = "Monday";
