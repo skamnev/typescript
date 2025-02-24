56d3c59d07eb812c2d23b8d9604167ba10f89105
Serhii Kamniev <sergey.kamnev@gmail.com>
2025-02-24 17:08:14 +0200

    Working with generiks in TS

--- classesrecap/src/generics.ts
+++ classesrecap/src/generics.ts
@@ -1,5 +1,5 @@
 //const nums: number[] = [];
-const nums: Array<number> = [];
+//const nums: Array<number> = [];
 const colors: Array<string> = [];
 
 const inputEl = document.querySelector<HTMLInputElement>("#username")!;
@@ -31,3 +31,64 @@ function identity<Type>(item: Type): Type {
 identity<number>(7);
 identity<string>("hello");
 identity<Cat>({name: "Cat", breed: "breed"});
+
+function getRandomElemnt<T>(list: T[]): T {
+    const randIdx = Math.floor(Math.random() * list.length);
+
+    return list[randIdx];
+}
+
+getRandomElemnt<string>(["a", "b", "c"]);
+getRandomElemnt<number>([5, 6, 21, 34, 436, 342]);
+
+function merge<T extends object,U extends object>(obj1: T, obj2: U) {
+    return {
+        ...obj1,
+        ...obj2
+    }
+}
+
+const comboObj = merge({ name: "colt" }, { pets: ["blue", "elton"]});
+
+interface Lengthy {
+    length: number;
+}
+
+//Assign type for generic function
+function printDoubleLength<T extends Lengthy >(thing: T): number {
+    return thing.length *2;
+}
+
+//Assign type for generic function 2
+function printDoubleLength2(thing: Lengthy): number {
+    return thing.length *2;
+}
+
+//default type for generic function
+function makeEmtpyArray<T = number>(): T[] {
+    return []
+}
+
+const nums = makeEmtpyArray();
+const bools = makeEmtpyArray<boolean>();
+
+interface Song {
+    title: string;
+    artist: string;
+}
+
+interface Video {
+    title: string;
+    creator: string;
+    resolution: string;
+}
+
+class Playlist<T> {
+    public queue: T[] = [];
+    add(el: T) {
+        this.queue.push(el)
+    }    
+}
+
+const songs = new Playlist<Song>();
+const videos = new Playlist<Video>();
